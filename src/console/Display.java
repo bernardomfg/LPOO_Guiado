3a7f9496d06d020a875b06fe4dfa623cb3307398
Joao Santos <joaofloressantos@gmail.com>
2013-03-05 09:32:54 +0000

    Display working

--- src/console/Display.java
+++ src/console/Display.java
@@ -7,10 +7,16 @@ import logic.Sword;
 
 public class Display {
 	public static void print(char[][] maze, Hero h, Drake d, Sword s, Eagle e) {
-		char [][] temp = maze.clone();
-		temp[h.getY()][h.getX()]='H';
-		temp[d.getY()][d.getX()]='D';
-		temp[s.getY()][s.getX()]='E';
+		int N = maze.length;
+		char[][] temp = new char[N][N];
+
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < N; j++) {
+				temp[i][j] = maze[i][j];
+			}
+		temp[h.getY()][h.getX()] = 'H';
+		temp[d.getY()][d.getX()] = 'D';
+		temp[s.getY()][s.getX()] = 'E';
 		for (char[] line : temp) {
 			for (char sym : line) {
 				System.out.print(sym);
