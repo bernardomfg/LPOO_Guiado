0156900f0e06d81c23ace67a15777e896d62dafd
Wilson Oliveira <wily1993@gmail.com>
2013-04-03 08:45:58 +0100

    Gui started. Choosing of game mode possible (not complete)

--- src/gui/Gui.java
+++ src/gui/Gui.java
@@ -1,12 +1,33 @@
 package gui;
 
+import java.awt.Dialog;
 import java.awt.EventQueue;
 
+import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import logic.Game;
+import logic.Hero;
+import logic.Maze;
+import logic.Sword;
+import javax.swing.JRadioButton;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import java.awt.GridLayout;
+import javax.swing.AbstractAction;
+import java.awt.event.ActionEvent;
+import javax.swing.Action;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JTextArea;
+import javax.swing.JRadioButtonMenuItem;
 
 public class Gui {
 
 	private JFrame frame;
+	private JDialog dialog;
 
 	/**
 	 * Launch the application.
@@ -22,6 +43,14 @@ public class Gui {
 				}
 			}
 		});
+		
+		Maze m = new Maze();
+		Hero h = new Hero();
+		Sword s = new Sword();
+		String mov;
+		Boolean gameOver = false;
+		
+	
 	}
 
 	/**
@@ -36,8 +65,26 @@ public class Gui {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 819, 548);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnNewButton = new JButton("New Game");
+		btnNewButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			
+				SelectMode chooseButton = new SelectMode();
+				chooseButton.setModalityType(Dialog.ModalityType.APPLICATION_MODAL);
+				chooseButton.setVisible(true);
+				//Game.gameMode = Integer.parseInt(chooseButton.op);
+			}
+		});
+		btnNewButton.setBounds(321, 444, 147, 51);
+		frame.getContentPane().add(btnNewButton);
+		
+			
+		
 
+	}
 }
---
+++ src/gui/SelectMode.java
@@ -0,0 +1,94 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JRadioButtonMenuItem;
+import javax.swing.JRadioButton;
+import javax.swing.JMenuItem;
+import javax.swing.ButtonGroup;
+import javax.swing.JLabel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import logic.Game;
+
+public class SelectMode extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private final ButtonGroup buttonGroup = new ButtonGroup();
+	public String op;
+
+	
+
+	/**
+	 * Create the dialog.
+	 */
+	public SelectMode() {
+		setBounds(100, 100, 565, 398);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		{
+			JRadioButtonMenuItem rdbtnmntmNewRadioItem = new JRadioButtonMenuItem("1");
+			rdbtnmntmNewRadioItem.setSelected(true);
+			buttonGroup.add(rdbtnmntmNewRadioItem);
+			rdbtnmntmNewRadioItem.setBounds(36, 89, 328, 37);
+			contentPanel.add(rdbtnmntmNewRadioItem);
+		}
+		{
+			JRadioButtonMenuItem rdbtnmntmDragao = new JRadioButtonMenuItem("2");
+			buttonGroup.add(rdbtnmntmDragao);
+			rdbtnmntmDragao.setBounds(36, 135, 328, 37);
+			contentPanel.add(rdbtnmntmDragao);
+		}
+		
+		JRadioButtonMenuItem rdbtnmntmDragao_1 = new JRadioButtonMenuItem("3");
+		buttonGroup.add(rdbtnmntmDragao_1);
+		rdbtnmntmDragao_1.setBounds(36, 183, 347, 37);
+		contentPanel.add(rdbtnmntmDragao_1);
+		
+		JLabel lblModoDeJogo = new JLabel("Modo de Jogo:");
+		lblModoDeJogo.setBounds(36, 24, 234, 37);
+		contentPanel.add(lblModoDeJogo);
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				
+				okButton.addMouseListener(new MouseAdapter() {
+					@Override
+					public void mouseClicked(MouseEvent e) {
+						
+						//Game.gameMode = Integer.parseInt(buttonGroup.getSelection().toString());
+						dispose();
+						
+					}
+					
+				});
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.addMouseListener(new MouseAdapter() {
+					@Override
+					public void mouseClicked(MouseEvent e) {
+						
+						dispose();
+					}
+				});
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+}
